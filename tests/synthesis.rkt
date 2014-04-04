39e8bd441e1094f6fb8f28acb2e0489a502217c6
mangpo <mangpo@eecs.berkeley.edu>
2014-04-03 18:35:33 -0700

    comment

--- tests/synthesis.rkt
+++ tests/synthesis.rkt
@@ -3,16 +3,19 @@
 (define-symbolic i0 i1 number?)
 (configure [bitwidth 32])
 
-(define (n?)
-  (define-symbolic* num number?)
-  num)
-
+;; no need to generate
 (define-syntax-rule (either a ...)
   (choose (list a ...)))
 
 (define (choose lst)
   (define-symbolic* choice number?)
   (list-ref lst choice))
+
+;; generate these
+(define (n?)
+  (define-symbolic* num number?)
+  (assert (and (< num 10) (>= num -10)))
+  num)
 
 (define (f x y) (+ x y 3))
 
@@ -27,10 +30,13 @@
     (define-symbolic* is-var boolean?)
     (if is-var (v?) (n?)))
     
-  (either (+ (??) (??) (??)) (- (??) (??) (??)) (* (??) (??) (??))))
+  (either (+ (??) (??) (??)) 
+          (- (??) (??) (??)) 
+          (* (??) (??) (??))))
 
 (define model
 (synthesize
   #:forall (list i0 i1)
+  ;#:assume (assert (and (= i0 1) (= i1 2)))
   #:assume (assert (and (< i0 10000) (> i0 -10000) (< i1 10000) (> i1 -10000)))
   #:guarantee (assert (eq? (f i0 i1) (g i0 i1)))))

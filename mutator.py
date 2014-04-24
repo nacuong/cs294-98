6800d43ad2ba566ba6eeb78508f85400fdddab1c
Cuong Nguyen <nacuong@live.com>
2014-04-24 14:16:22 -0700

    Added two more mutators: PreserverStructureAndOp and PreserveStructure

--- mutator.py
+++ mutator.py
@@ -1,6 +1,64 @@
 import ast, copy
 from synthesis_ast import Either, AllNum, AllVar, AllNumVar
 
+class PreserverStructure(ast.NodeVisitor):
+  def visit_BinOp(self, node):
+    left = None
+    op = None
+    right = None
+    for field, value in ast.iter_fields(node): 
+      if field == "left":
+        left = value
+      elif field == "op":
+        op = value
+      elif field == "right":
+        right = value
+
+    add = ast.BinOp(self.visit(left), ast.Add(), self.visit(right), lineno = 0,
+        col_offset = 0)
+    sub = ast.BinOp(self.visit(left), ast.Sub(), self.visit(right), lineno = 0,
+        col_offset = 0)
+    mult = ast.BinOp(self.visit(left), ast.Mult(), self.visit(right), lineno = 0,
+        col_offset = 0)
+    div = ast.BinOp(self.visit(left), ast.Div(), self.visit(right), lineno = 0,
+        col_offset = 0)
+
+    return Either([add,sub,mult,div])
+
+  def visit_Num(self, node):
+    return AllNum()
+
+  def visit_Name(self, node):
+    return AllVar()
+
+  def generic_visit(seld, node):
+    return node
+
+class PreserveStructureAndOp(ast.NodeVisitor):
+  def visit_BinOp(self, node):
+    left = None
+    op = None
+    right = None
+    for field, value in ast.iter_fields(node): 
+      if field == "left":
+        left = value
+      elif field == "op":
+        op = value
+      elif field == "right":
+        right = value
+
+    return ast.BinOp(self.visit(left), op, self.visit(right), lineno = 0,
+        col_offset = 0)
+
+  def visit_Num(self, node):
+    return AllNum()
+
+  def visit_Name(self, node):
+    return AllVar()
+
+  def generic_visit(seld, node):
+    return node
+
 class OffByOne(ast.NodeVisitor):
 
   def generic_visit(self, node):
